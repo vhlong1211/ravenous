c8fe2d3196fa39d673fac49eedeb3b6d21f8bfc1
Vh Long <vhlong@Vhs-MacBook-Air.local>
2020-08-13 09:51:41 +0700

    {lastUpdate}

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import './App.css';
-import Business from '../Business/Business.js';
+//import Business from '../Business/Business.js';
 import SearchBar from '../SearchBar/SearchBar.js';
 import BusinessList from '../BusinessList/BusinessList.js';
-
-let business={
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 900
-}
-let businesses=[business,business,business,business,business,business];
+import Yelp from '../../util/Yelp.js';
+// let business={
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 900
+// }
+// let businesses=[business,business,business,business,business,business];
 class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      businesses:[]
+    }
+    this.searchYelp=this.searchYelp.bind(this)
+  }
+  searchYelp(term,location,sortBy){
+    Yelp.search(term,location,sortBy).then((businesses)=>{
+      this.setState({
+        businesses:businesses
+      })
+    })
+  }
   render(){
     return(
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
-        <BusinessList businesses={businesses}/> 
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={this.state.businesses}/> 
       </div>
     )
   }
